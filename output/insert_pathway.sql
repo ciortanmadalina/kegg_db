405fbdbd8e62d38d6bfd7bfc8faf0944488cc244
ciortanmadalina <ciortanmadalina@gmail.com>
2016-12-15 14:02:59 -0800

    added schema

---
+++ output/insert_pathway.sql
@@ -0,0 +1,3 @@
+INSERT INTO raw_pathway(name, description) VALUES ('path:map00010', 'Glycolysis');
+INSERT INTO raw_pathway(name, description) VALUES ('path:map00020', 'Citrate');
+INSERT INTO raw_pathway(name, description) VALUES ('path:map00030', 'Pentose');
--- schema.sql
+++ schema.sql
@@ -2,26 +2,25 @@ CREATE TABLE raw_pathway (
     name varchar (150) PRIMARY KEY,
     description varchar (200));
   
-CREATE TABLE raw_module (
-    name varchar (150) PRIMARY KEY,
-    description varchar (200));
-  
+CREATE TABLE raw_module_reaction(
+    module varchar (10),
+    reaction varchar (10)
+);
+
+CREATE TABLE raw_reaction_compound (
+    reaction varchar(10), 
+    compound varchar(10), 
+    type varchar(10));
+
+
 CREATE TABLE raw_pathway_module (
     pathway varchar (150),
     module varchar (150));
   
+CREATE TABLE raw_compound (
+    id varchar (10),
+    name varchar(200),
+    formula varchar(100),
+    mass varchar (100));
   
-CREATE TABLE pathway (
-  id serial PRIMARY KEY,
-    name varchar (150),
-    description varchar (200));
-  
-CREATE TABLE module (
-  id serial PRIMARY KEY,
-    name varchar (150),
-    description varchar (200));
-  
-CREATE TABLE pathway_module (
-  id serial PRIMARY KEY,
-    pathway NUMERIC,
-    module NUMERIC );
+

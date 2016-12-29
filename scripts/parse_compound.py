e881735ca6c73273c0e9157758adc433fb77739e
ciortanmadalina <ciortanmadalina@gmail.com>
2016-12-29 11:50:57 -0800

    escaped compounds

--- scripts/parse_compound.py
+++ scripts/parse_compound.py
@@ -2,11 +2,6 @@
 import re
 import sys
 import utils
-#print ('Number of arguments:', len(sys.argv), 'arguments.', str(sys.argv))
-
-if len(sys.argv) != 2 :
-    print('You provided ' ,(len(sys.argv) - 1 ) , ' arguments. \n Please run parse_module.py <module_name>')
-    sys.exit(2)
 
 compound = sys.argv[1]
 compoundsFileName = 'output/insert_compound.sql'
@@ -21,7 +16,7 @@ mass=utils.getSectionString("EXACT_MASS",data)
 
 compoundsFile = open (compoundsFileName, 'a')
 compoundsFile.write(
-"INSERT INTO raw_compound (id, name, formula, mass) VALUES ('" + compound + "' , '" + names + "' , '"+ formula  + "' , '" +  mass + "');\n")
+"INSERT INTO raw_compound (id, name, formula, mass) VALUES (\"" + compound + "\" , \"" + names + "\" , \""+ formula  + "\" , \"" +  mass + "\");\n")
 
 compoundsFile.close()
 
